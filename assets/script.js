c601f62ba7ba88a216214229c1affc6af33cc615
cloud444 <y_michael@outlook.com>
2024-02-01 21:34:19 +1100

    change style, added bold text

--- assets/script.js
+++ assets/script.js
@@ -27,3 +27,7 @@ console.log('Here are the topics we learned through Prework:');
 listTopics();
 console.log('Which topic should we study first?');
 selectTopic();
+
+function highlight() {
+    
+}
